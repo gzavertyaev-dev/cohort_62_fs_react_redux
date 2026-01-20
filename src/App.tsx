a4fe218148147d147be2f387905a559129f34149
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2026-01-20 20:35:06 +0100

    add lesson 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 // Lessons
 import Lesson_17 from "lessons/Lesson_17/Lesson_17"
 
+// Homeworks
+import Homework_17 from "homeworks/Homework_17/Homework_17"
+
 function App() {
   return (
     <>
       <Lesson_17 />
+      {/* <Homework_17 /> */}
     </>
   )
 }
--- src/store/redux/counterSlice/counterSlice.ts
+++ src/store/redux/counterSlice/counterSlice.ts
@@ -1,6 +1,10 @@
+import { PayloadAction } from "@reduxjs/toolkit"
+
 import { createAppSlice } from "store/createAppSlice"
 
-const counterInintialState = {
+import { CounterSliceState } from "./types"
+
+const counterInintialState: CounterSliceState = {
   count: 0,
 }
 
@@ -11,16 +15,23 @@ export const counterSlice = createAppSlice({
   initialState: counterInintialState,
   // reducers - это обьект, который содержит функции редьюсер, которыен в свою очередь будут изменять стейт
   reducers: {
-    plus: state => {
+    plus: (state: CounterSliceState) => {
       state.count = state.count + 1 // если state.count === 0, то state.count = 0 + 1
     },
-    minus: state => {
+    minus: (state: CounterSliceState) => {
       state.count = state.count - 1
+    },
+    multiply: (state: CounterSliceState, action: PayloadAction<number>) => {
+      console.log(action)
+      state.count = Number((state.count * action.payload).toFixed(3))
+    },
+    divide: (state: CounterSliceState, action: PayloadAction<number>) => {
+      state.count = Number((state.count / action.payload).toFixed(2))
     },
   },
   // selectors - мы прописываем, какие именно данные мы хотим отдать компонентам
   selectors: {
-    count: state => {
+    count: (state: CounterSliceState) => {
       return state.count
     },
   },
